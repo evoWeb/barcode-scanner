325e3e8ca9d2aa3e8f89c3cb632ae28bbf9188e5
Sebastian Fischer <typo3@evoweb.de>
2016-05-06 16:06:18 +0200

    Restructuring

--- Resources/JavaScipt/app.js
+++ Resources/JavaScipt/app.js
@@ -1,3 +1,4 @@
+/* globals module, define, require, process */
 (function (root, factory) {
 	'use strict';
 	if (typeof module !== 'undefined' && module.exports) {
@@ -31,48 +32,59 @@
 	};
 
 	function initialize() {
+		module.registerNewsElement();
+
 		$(document).ready(function () {
 			module.container = $(module.namespace);
-			module.video = $('video:eq(0)', module.container)[0];
-			module.canvas = module.getCanvas();
-			module.context = module.canvas[0].getContext('2d');
+			module.getVideo();
+			module.getCanvas();
 
-			var video = $(module.video);
-
-			// this fixed that with high resolution webcam the canvas gets only a portion of the capture
-			module.canvas.attr('width', module.canvas.width());
-			module.canvas.attr('height', module.canvas.height());
-			video.attr('width', video.width());
-			video.attr('height', video.height());
-
+			module.addStartStopButton();
 			module.attachVideoCapture();
-
-			$(document).on('click', module.namespace + ' .startStop', function () {
-				if (module.streamRunning) {
-					module.detachVideoCapture();
-				} else {
-					module.attachVideoCapture();
-				}
-			});
-
-			$(document).on('click', module.namespace + ' .capture', function () {
-				module.context.drawImage(module.video, 0, 0, module.canvas.width(), module.canvas.height());
-			});
 		});
 	}
 
-	module.getCanvas = function () {
-		var $canvas = $('canvas:eq(0)', module.namespace);
-
-		if ($canvas.length == 0) {
-			$canvas = $('<canvas/>');
-		}
-
-		return $canvas;
+	module.registerNewsElement = function() {
+		module.element = root.document.registerElement('evoweb-barcode-scanner');
 	};
 
+	module.getVideo = function () {
+		var $video = module.container.find('video:eq(0)');
+
+		if ($video.length === 0) {
+			$video = $('<video/>');
+			module.container.append($video);
+		}
+
+		module.video = $video;
+		module.video.attr('width', module.video.width());
+		module.video.attr('height', module.video.height());
+	};
+
+	module.getCanvas = function () {
+		var $canvas = module.container.find('canvas:eq(0)');
+
+		if ($canvas.length === 0) {
+			$canvas = $('<canvas/>');
+			module.container.append($canvas);
+		}
+
+		module.canvas = $canvas;
+		module.canvas.attr('width', module.canvas.width());
+		module.canvas.attr('height', module.canvas.height());
+
+		module.context = $canvas[0].getContext('2d');
+
+		module.context.beginPath();
+		module.context.moveTo(0, module.canvas.height() / 2);
+		module.context.lineTo(module.canvas.width(), module.canvas.height() / 2);
+		module.context.stroke();
+	};
+
+
 	module.attachVideoCapture = function () {
-		var videoObj = {"video": true},
+		var video = module.video[0],
+			videoObj = {"video": true},
 			errBack = function (error) {
 				console.log("Video capture error: ", error.code);
 			};
@@ -81,34 +93,50 @@
 		if (navigator.getUserMedia) { // Standard
 			navigator.getUserMedia(videoObj, function (localStream) {
 				module.stream = localStream;
-				module.video.src = localStream;
-				module.video.play();
+				video.src = localStream;
+				video.play();
 			}, errBack);
 		} else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
 			navigator.webkitGetUserMedia(videoObj, function (localStream) {
 				module.stream = localStream;
-				module.video.src = root.URL.createObjectURL(localStream);
-				module.video.play();
+				video.src = root.URL.createObjectURL(localStream);
+				video.play();
 			}, errBack);
 		} else if (navigator.mozGetUserMedia) { // Firefox-prefixed
 			navigator.mozGetUserMedia(videoObj, function (localStream) {
 				module.stream = localStream;
-				module.video.src = root.URL.createObjectURL(localStream);
-				module.video.play();
+				video.src = root.URL.createObjectURL(localStream);
+				video.play();
 			}, errBack);
 		}
 		module.streamRunning = true;
 	};
 
 	module.detachVideoCapture = function () {
-		module.video.src = '';
-		module.video.pause();
+		var video = module.video[0];
+
+		video.src = '';
+		video.pause();
 
 		var track = module.stream.getTracks()[0];
 		track.stop();
 
 		module.streamRunning = false;
 	};
+
+
+	module.addStartStopButton = function() {
+		var $startStop = $('<button class="startStop">Toggle video</button>').on('click', function () {
+			if (module.streamRunning) {
+				module.detachVideoCapture();
+			} else {
+				module.attachVideoCapture();
+			}
+		});
+
+		module.container.after($startStop);
+	};
+
 
 	initialize();
 
